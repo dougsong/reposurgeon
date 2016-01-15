bbba9706b4480b4eaf37c61fd0017d2d7e0d1126
Kevin Caswick <KCaswick@wspackaging.com>
2016-01-15 10:59:46 -0600

    Added test script for repotool checkout from CVS repository.
    Still need to add .chk file once errors in repotool are fixed.

---
+++ test/repotool-checkout-cvs.sh
@@ -0,0 +1,20 @@
+## Test repotool checkout of git repo
+
+command -v cvs >/dev/null 2>&1 || { echo "    Skipped, cvs missing."; exit 0; }
+
+trap "rm -rf /tmp/test-repo$$ /tmp/target$$" 0 12 2 15
+
+cp -r hack1.repo/ /tmp/test-repo$$
+cd /tmp/test-repo$$
+repotool checkout /tmp/target$$; echo Return code: $? >/tmp/out$$
+cd ~-
+./dir-md5 /tmp/target$$  >>/tmp/out$$
+
+case $1 in
+    --regress)
+        diff --text -u $2.chk /tmp/out$$ || exit 1; ;;
+    --rebuild)
+	cat /tmp/out$$ >$2.chk;;
+esac
+
+#end
