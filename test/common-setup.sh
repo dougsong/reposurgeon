58d5ec9af213771ae1050230107d8c6458116bde
Eric Sunshine <sunshine@sunshineco.com>
2017-03-09 09:46:16 -0500

    test: make functional on platforms lacking 'realpath' command
    Some tests change working directory but also need to know the locations
    of various resources. Rather than trying to compute those locations
    relative to the working directory, they utilize 'realpath' to obtain
    absolute paths.
    
    However, some platforms (such as stock MacOS) lack 'realpath'.
    Detect this shortcoming and fake up a 'realpath' command (which,
    while not a fully functional replacement, provides sufficient
    functionality to satisfy the test scripts).
    
    Note: This faux 'realpath' uses $PWD, but $PWD does not seem to be
    set in GitLab's CI environment, however the CI does have 'realpath',
    so this use of $PWD as a fallback for missing 'realpath' does no harm.
    
    Signed-off-by: Eric Sunshine <sunshine@sunshineco.com>

---
+++ test/common-setup.sh
@@ -0,0 +1,6 @@
+# Not all platforms have a 'realpath' command, so fake one up if needed
+# using $PWD.
+# Note: GitLab's CI environment does not seem to define $PWD, however, it
+# does have 'realpath', so use of $PWD here does no harm.
+command -v realpath >/dev/null 2>&1 ||
+    realpath() { test -z "${1%%/*}" && echo "$1" || echo "$PWD/${1#./}"; }
