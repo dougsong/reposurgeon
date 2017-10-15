23744f0fde41c08af1733fe987cac5a180d0afc9
Eric S. Raymond <esr@thyrsus.com>
2017-10-15 11:45:57 -0400

    Code, documentation, and tesrts fot "incorporate" command.

---
+++ test/misc-incorporate1.sh
@@ -0,0 +1,17 @@
+## Test incorporate command - before case
+
+trap "rm -fr /tmp/sub$$ tarball.tar" 0 12 2 15
+
+mkdir /tmp/sub$$
+
+# Build a tarball to be incorporated
+mkdir /tmp/sub$$/tarball
+echo "first sample small file" >/tmp/sub$$/tarball/snip
+echo "second sample small file" >/tmp/sub$$/tarball/snap
+chmod a+x /tmp/sub$$/tarball/snap
+here=`pwd`
+(cd /tmp/sub$$; tar cf tarball.tar tarball)
+mv /tmp/sub$$/tarball.tar .
+
+# Incorporate it
+reposurgeon "echo 1" "read <min.fi" "@min(=C) incorporate tarball.tar" "write -"
