515f741f312a7bfe50ab125d2b0d14139a2f2f9e
Eric S. Raymond <esr@thyrsus.com>
2017-12-22 00:40:36 -0500

    Partial translation of repotool to Python.

--- test/repotool-checkout-hg-tag.sh
+++ test/repotool-checkout-hg-tag.sh
@@ -7,7 +7,7 @@ trap "rm -rf /tmp/test-repo$$ /tmp/target$$ /tmp/out$$" 0 12 2 15
 
 ./hg-to-fi -n /tmp/test-repo$$ < simple.fi
 cd /tmp/test-repo$$
-repotool checkout /tmp/target$$ lightweight-sample
+repotool checkout -t lightweight-sample /tmp/target$$
 echo Return code: $? >/tmp/out$$
 cd - >/dev/null
 ./dir-md5 /tmp/target$$ >>/tmp/out$$
