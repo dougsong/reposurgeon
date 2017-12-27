94c94690dc14994f6f2e80412bcce6ebb41a4ef1
Eric S. Raymond <esr@thyrsus.com>
2017-12-26 19:32:18 -0500

    Test script cleanup.

--- test/repotool-compare-tag-svn-git.sh
+++ test/repotool-compare-tag-svn-git.sh
@@ -4,15 +4,18 @@ command -v svn >/dev/null 2>&1 || { echo "    Skipped, svn missing."; exit 0; }
 command -v git >/dev/null 2>&1 || { echo "    Skipped, git missing."; exit 0; }
 
 # Results should be independent of what file stem this is, as
-# long as it's an svn dump and.
+# long as it's an svn dump and has the right festure to be named by cmploc.
 stem=simpletag
-cmptag=tag1
+cmploc=tag1
+cmpmode=-t
+
+# No user-serviceable parts below this line
 
 trap "rm -rf /tmp/test-repo$$-svn /tmp/test-repo$$-git /tmp/out$$" 0 12 2 15
 
 ./svn-to-svn -q -c /tmp/test-repo$$-svn <${stem}.svn
 reposurgeon "read <${stem}.svn" "prefer git" "rebuild /tmp/test-repo$$-git" >/tmp/out$$ 2>&1
-repotool compare -t ${cmptag} /tmp/test-repo$$-svn-checkout /tmp/test-repo$$-git | sed -e "s/$$/\$\$/"g >>/tmp/out$$
+repotool compare ${cmpmode} ${cmploc} /tmp/test-repo$$-svn-checkout /tmp/test-repo$$-git | sed -e "s/$$/\$\$/"g >>/tmp/out$$
 
 
 case $1 in
