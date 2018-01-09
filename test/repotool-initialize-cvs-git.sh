a702f45ced6b3c112a0ff50a33b952ec37fc0f4a
Eric Siegerman <pub08-git@davor.org>
2018-01-09 18:54:37 -0500

    Add tests for "repotool initialize"
    These tests are broken for the moment: they pass, but only by expecting
    the buggy status quo ante.

---
+++ test/repotool-initialize-cvs-git.sh
@@ -0,0 +1,25 @@
+## Test repotool initialize with cvs source and git dest
+
+mkdir /tmp/test-workdir$$
+cd /tmp/test-workdir$$
+repotool initialize xyzzy cvs git >/tmp/out$$
+echo Return code: $? >>/tmp/out$$
+cd - >/dev/null
+./dir-md5 /tmp/test-workdir$$ >>/tmp/out$$
+
+case $1 in
+    --regress)
+        diff --text -u $2.chk /tmp/out$$ || exit 1; ;;
+    --rebuild)
+	cat /tmp/out$$ >$2.chk;;
+esac
+	      
+st=$?
+if [ $st -eq 0 ]; then
+	rm -rf /tmp/test-workdir$$ /tmp/out$$
+fi
+
+exit $st
+
+#end
+
