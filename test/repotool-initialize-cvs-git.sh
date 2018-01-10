9d70c776a217cf872a338f435e7c2d33de13cba5
Eric Siegerman <pub08-git@davor.org>
2018-01-09 21:05:03 -0500

    Add support for "--view"
    ... which somehow got lost in translation.

--- test/repotool-initialize-cvs-git.sh
+++ test/repotool-initialize-cvs-git.sh
@@ -12,8 +12,10 @@ case $1 in
         diff --text -u $2.chk /tmp/out$$ || exit 1; ;;
     --rebuild)
 	cat /tmp/out$$ >$2.chk;;
+    --view)
+	cat /tmp/out$$;;
 esac
-	      
+
 st=$?
 if [ $st -eq 0 ]; then
 	rm -rf /tmp/test-workdir$$ /tmp/out$$
