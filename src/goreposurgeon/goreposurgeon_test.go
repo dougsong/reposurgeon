20a6b1c5eac84b1ac8249d2cae735c999ec70f4b
Eric S. Raymond <esr@thyrsus.com>
2018-10-08 21:09:33 -0400

    Unit test for basic deletion case.

--- src/goreposurgeon/goreposurgeon_test.go
+++ src/goreposurgeon/goreposurgeon_test.go
@@ -38,6 +38,8 @@ Things that still need unit tests:
 
 * Eric Sunshine's tests with malformed author lines.
 
+* Deletion cases other than default (--tagforward pushforward).
+
 */
 
 func assertBool(t *testing.T, see bool, expect bool) {
@@ -1190,8 +1192,8 @@ woc = wocwoc <woc@cow>
 	repo.cleanup()
 }
 
-func TestFastImportParse2(t *testing.T) {
-	rawdump := `blob
+// Sample small repository used for multiple tests
+const rawdump = `blob
 mark :1
 data 23
 This is a sample file.
@@ -1266,6 +1268,8 @@ data 19
 this is a test tag
 
 `
+
+func TestFastImportParse2(t *testing.T) {
 	repo := newRepository("test")
 	sp := newStreamParser(repo)
 	r := strings.NewReader(rawdump)
@@ -1360,6 +1364,82 @@ M 100644 :3 README
 	assertBool(t, ancestors.Equal(orderedIntSet{4, 2}), true)
 
 	repo.cleanup()
+}
+
+func TestDelete(t *testing.T) {
+	repo := newRepository("test")
+	sp := newStreamParser(repo)
+	r := strings.NewReader(rawdump)
+	sp.fastImport(r, nil, false, "synthetic test load")
+
+	thirdcommit := repo.find(":6")
+	repo.delete(orderedIntSet{thirdcommit}, nil)
+	var a strings.Builder
+	if err := repo.fastExport(repo.all(), &a,
+		newStringSet(), nil, false); err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+
+	dtrimmed := `blob
+mark :1
+data 23
+This is a sample file.
+
+reset refs/heads/master
+commit refs/heads/master
+mark :2
+committer esr <esr> 1322671432 +0000
+data 16
+First revision.
+M 100644 :1 README
+
+blob
+mark :3
+data 68
+This is a sample file.
+
+This is our first line of modified content.
+
+commit refs/heads/master
+mark :4
+committer esr <esr> 1322671521 +0000
+data 17
+Second revision.
+from :2
+M 100644 :3 README
+
+tag root
+from :2
+tagger esr <esr> 1322671315 +0000
+data 122
+A vanilla repository - standard layout, linear history, no tags, no branches. 
+
+[[Tag from root commit at Subversion r1]]
+
+tag emptycommit-5
+from :6
+tagger esr <esr> 1323084440 +0000
+data 151
+Adding a property setting.
+
+[[Tag from zero-fileop commit at Subversion r5:
+<NodeAction: r5 change file 'trunk/README' properties=[('foo', 'bar')]>
+]]
+
+tag no-comment
+from :4
+tagger esr <esr> 1322671316 +0000
+data 0
+
+tag with-comment
+from :6
+tagger esr <esr> 1322671317 +0000
+data 19
+this is a test tag
+
+`
+	assertEqual(t, a.String(), dtrimmed)
 }
 
 // end
