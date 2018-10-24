4f76bbd0a872fcdb1109799fd0398496537dd522
Eric S. Raymond <esr@thyrsus.com>
2018-10-23 23:07:00 -0400

    Add setattr implementation in Go.

--- src/goreposurgeon/goreposurgeon.go
+++ src/goreposurgeon/goreposurgeon.go
@@ -247,6 +247,36 @@ func hasAttr(obj interface{}, fld string) bool {
 	return ok
 }
 
+// setAttr emulates Python setattr using the Go reflection system.  It
+// sets the provided obj field with provided value. obj param has to
+// be a pointer to a struct, otherwise it will soundly fail. Provided
+// value type should match with the struct field you're trying to set.
+// Code swiped from https://github.com/oleiade/reflections/
+func setAttr(obj interface{}, name string, value interface{}) error {
+	// Fetch the field reflect.Value
+	structValue := reflect.ValueOf(obj).Elem()
+	structFieldValue := structValue.FieldByName(name)
+
+	if !structFieldValue.IsValid() {
+		return fmt.Errorf("No such field: %s in obj", name)
+	}
+
+	// If obj field value is not settable an error is thrown
+	if !structFieldValue.CanSet() {
+		return fmt.Errorf("Cannot set %s field value", name)
+	}
+
+	structFieldType := structFieldValue.Type()
+	val := reflect.ValueOf(value)
+	if structFieldType != val.Type() {
+		invalidTypeError := errors.New("Provided value type didn't match obj field type")
+		return invalidTypeError
+	}
+
+	structFieldValue.Set(val)
+	return nil
+}
+
 // This representation optimizes for small memory footprint at the expense
 // of speed.  To make the opposite trade we would do the obvious thing with
 // map[string] bool.
