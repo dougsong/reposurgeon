80360cef18a18b206417a82ba02daadf8a1c7aff
Eric S. Raymond <esr@thyrsus.com>
2019-11-03 11:22:29 -0500

    Allow xommas as a non-witespace separator in set/clear/log.

--- surgeon/reposurgeon.go
+++ surgeon/reposurgeon.go
@@ -20790,6 +20790,7 @@ func tweakFlagOptions(line string, val bool) {
 			respond("\t%s = %v\n", opt[0], control.flagOptions[opt[0]])
 		}
 	} else {
+		line = strings.ReplaceAll(line, ",", " ")
 		for _, name := range strings.Fields(line) {
 			for _, opt := range optionFlags {
 				if name == opt[0] {
@@ -21839,6 +21840,7 @@ func verbosityLevelList() []assoc {
 }
 
 func (rs *Reposurgeon) DoLog(lineIn string) bool {
+	lineIn = strings.ReplaceAll(lineIn, ",", " ")
 	for _, tok := range strings.Fields(lineIn) {
 		enable := tok[0] == '+'
 		if !(enable || tok[0] == '-') {
