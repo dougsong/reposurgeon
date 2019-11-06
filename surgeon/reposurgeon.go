dougsong/reposurgeon

commit a33ad3b3d68e9788cb2e252f6ef93cf206fe255f

the DoChangelogs command doesn't need the textual diff output
It can use the data structures created by difflib instead.

surgeon/reposurgeon.go
@@ -21202,7 +21202,6 @@ func (rs *Reposurgeon) DoChangelogs(line string) bool {
 		if !notChangelog {
 			return
 		}
-		differ := difflib.NewDiffer()
 		for _, op := range commit.operations() {
 			if op.op == opM && filepath.Base(op.Path) == "ChangeLog" {
 				cl++
@@ -21216,17 +21215,16 @@ func (rs *Reposurgeon) DoChangelogs(line string) bool {
 				now := strings.Split(string(newcontent), "\n")
 				var attribution string
 				//print("Analyzing Changelog at %s." % commit.mark)
-				comparison, err := differ.Compare(then, now)
-				if err != nil {
-					logit(logSHOUT, "differ threw a should-never-happen error on path %s at %s", op.Path, commit.mark)
-					return
-				}
-				for _, diffline := range comparison {
-					if diffline[0] == '+' || diffline[0] == '?' {
-						newAttribution := parseAttributionLine(diffline[2:])
-						if newAttribution != "" {
-							attribution = newAttribution
-							break
+				differ := difflib.NewMatcherWithJunk(then, now, true, nil)
+				comparison := differ.GetOpCodes()
+				for _, difflines := range comparison {
+					if difflines.Tag == 'i' || difflines.Tag == 'r' {
+						for _, diffline := range now[difflines.J1:difflines.J2] {
+							newAttribution := parseAttributionLine(diffline)
+							if newAttribution != "" {
+								attribution = newAttribution
+								break
+							}
 						}
 					}
 				}
@@ -21238,10 +21236,11 @@ func (rs *Reposurgeon) DoChangelogs(line string) bool {
 					// assume the most recent is the latest and
 					// correct. First count the lines before the
 					// first change...
-					var commonCount int;
-					var diffline string
-					for commonCount, diffline = range comparison {
-						if diffline[0] != ' ' {
+					var commonCount int
+					for _, difflines := range comparison {
+						if difflines.Tag == 'e' {
+							commonCount = difflines.I2
+						} else {
 							break
 						}
 					}

commit da4dd2131b7ec33a00e7ded8a12f0089ac1fa01a

Really break out of the intended loop
Since a33ad3b3 introduced a new inner loop, the break command no longer
escapes the loop it was intended to. Use a goto instead, and while we
are at it, jump past the fallback search for an attribution.

surgeon/reposurgeon.go
@@ -21222,6 +21222,7 @@ func (rs *Reposurgeon) DoChangelogs(line string) bool {
 				newcontent := repo.markToEvent(op.ref).(*Blob).getContent()
 				now := strings.Split(string(newcontent), "\n")
 				var attribution string
+				var commonCount int
 				//print("Analyzing Changelog at %s." % commit.mark)
 				differ := difflib.NewMatcherWithJunk(then, now, true, nil)
 				comparison := differ.GetOpCodes()
@@ -21231,41 +21232,38 @@ func (rs *Reposurgeon) DoChangelogs(line string) bool {
 							newAttribution := parseAttributionLine(diffline)
 							if newAttribution != "" {
 								attribution = newAttribution
-								break
+								goto attributionFound
 							}
 						}
 					}
 				}
-				if attribution == "" {
-					// This is the tricky part.  We want the
-					// last attribution from before the change
-					// band to stick because there is none *in*
-					// the change band. If there's more than one,
-					// assume the most recent is the latest and
-					// correct. First count the lines before the
-					// first change...
-					var commonCount int
-					for _, difflines := range comparison {
-						if difflines.Tag == 'e' {
-							commonCount = difflines.I2
-						} else {
-							break
-						}
-					}
-					// Then parse the attributions in reverse order to
-					// avoid parsing and tossing away a lot of them.
-					for i := commonCount-1; i >= 0; i-- {
-						newAttribution := parseAttributionLine(now[i])
-						if newAttribution != "" {
-							attribution = newAttribution
-							break
-						}
+				// This is the tricky part.  We want the
+				// last attribution from before the change
+				// band to stick because there is none *in*
+				// the change band. If there's more than one,
+				// assume the most recent is the latest and
+				// correct. First count the lines before the
+				// first change...
+				for _, difflines := range comparison {
+					if difflines.Tag == 'e' {
+						commonCount = difflines.I2
+					} else {
+						break
 					}
 				}
-				if attribution != "" {
-					attributions[event_id] = append(attributions[event_id], attribution)
-					blobRefs[event_id] = append(blobRefs[event_id], op.ref)
+				// Then parse the attributions in reverse order to
+				// avoid parsing and tossing away a lot of them.
+				for i := commonCount-1; i >= 0; i-- {
+					newAttribution := parseAttributionLine(now[i])
+					if newAttribution != "" {
+						attribution = newAttribution
+						goto attributionFound
+					}
 				}
+				continue
+				attributionFound:
+				attributions[event_id] = append(attributions[event_id], attribution)
+				blobRefs[event_id] = append(blobRefs[event_id], op.ref)
 			}
 		}
 		control.baton.percentProgress(uint64(evts))