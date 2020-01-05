f2001a3ea2d56c8b03c161d16f0fda2d5ed2ed4c
Eric S. Raymond <esr@thyrsus.com>
2020-01-05 06:04:09 -0500

    Use packaged cvs-fast-export rather than building from the repo.

--- ci/prepare.sh
+++ ci/prepare.sh
@@ -1,36 +1,19 @@
 #!/bin/bash
 
-# Install dependencies that aren't included in buildpack-deps:jessie
-
-REPOSURGEON_DIR=$(pwd)
-
 apt-get update -qy && apt-get install -qy --no-install-recommends \
     asciidoc \
     asciidoctor \
-    bison \
     cvs \
-    flex \
+    cvs-fast-export \
     golang \
-    libpcre3-dev \
     mercurial \
     parallel \
-    pypy \
     python2.7 \
     python3 \
     subversion \
     time \
-    xmlto \
  && apt-get clean \
  && rm -rf /var/lib/apt/lists/*
-
-# Install cvs-fast-export - this is the only reason bison and flex are installed
-mkdir -p /usr/local/src/
-cd /usr/local/src/
-git clone https://gitlab.com/esr/cvs-fast-export.git && \
-    cd cvs-fast-export/ && \
-    make install
-
-cd $REPOSURGEON_DIR
 
 echo
 echo ============= Dependency install complete ============= 
