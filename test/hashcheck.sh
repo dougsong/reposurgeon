3a5b2a81d1832f9b56c372f2bb19e882c6d4cb90
Eric S. Raymond <esr@thyrsus.com>
2020-03-28 00:52:17 -0400

    Document the example used for hasheck testing.

--- test/hashcheck.sh
+++ test/hashcheck.sh
@@ -8,6 +8,22 @@ trap 'rm -fr ${repo}' EXIT HUP INT QUIT TERM
 
 command -v git >/dev/null 2>&1 || ( echo "$0: git is not installed"; exit 1 )
 
+#  Elijah Newren <newren@gmail.com>> Most of this is explained at
+# > https://git-scm.com/book/en/v2/Git-Internals-Git-Objects, though they
+# > don't really cover the internals of tree objects.  You can learn
+# > most of this stuff as well by playing around with the hash-object,
+# > mktree, commit-tree, write-tree, and cat-file subcommands of git, but
+# > it might be easiest to just demonstrate how to construct a full commit
+# > hash using shell commands and comparing to what git reports, and doing
+# > so a couple different ways for each step:
+# 
+# Very good.  I've made a start on hash computation.  It's in the public repo
+# at https://gitlab.com/esr/reposurgeon
+# 
+# I was able to follow your steps for blob hash computation and adapt
+# them into a test for blob hashing.  The new file test/hascheck.sh
+# looks like this:
+
 git init --quiet "${repo}"
 cd "${repo}" >/dev/null
 # Required for CI
@@ -40,4 +56,87 @@ then
 fi
 
 echo "hashcheck: PASSED"
+ 
+# This test passes and demonstrates that I have replicated git blob hashing
+# correctly.
+# 
+# The new reposurgeon "hash" commant is intended to take a selection set and
+# return for each hashable event a line of the form n: xxxxxxxx where n is
+# an event number and xxxxxxxx is the normal hex representation of its
+# SHA-1 hash.
+# 
+# Unfortunately, right now it only works for blobs, because I don't
+# understand the tree hash computation yet.  You wrote this:
+#  
+# > $ # Ask git for the inner tree hash, two different ways, then duplicate
+# > $ git rev-parse $(git write-tree):some
+# > 08687c1be8a39bde242c31d308baa4aba277dc02
+# > $ git rev-parse HEAD:some
+# > 08687c1be8a39bde242c31d308baa4aba277dc02
+# > $ printf "100644 blob
+# > 8ab686eafeb1f44702738c8b0f24f2567c36da6d\tfile.txt" | git mktree
+# > 08687c1be8a39bde242c31d308baa4aba277dc02
+# > $ (printf "tree 36\0"; printf "100644 file.txt\0"; echo -n
+# > 8ab686eafeb1f44702738c8b0f24f2567c36da6d | xxd -p -r) | sha1sum
+# > 08687c1be8a39bde242c31d308baa4aba277dc02  -
+# > 
+# > $ # Ask git for the outer tree hash, two different ways, then duplicate
+# > $ git write-tree
+# > 62296ca7563b5d575acb0a914442f78f3a76db4d
+# > $ git rev-parse HEAD^{tree}
+# > 62296ca7563b5d575acb0a914442f78f3a76db4d
+# > $ printf "040000 tree 08687c1be8a39bde242c31d308baa4aba277dc02\tsome"
+# > | git mktree
+# > 62296ca7563b5d575acb0a914442f78f3a76db4d
+# > $ (printf "tree 31\0"; printf "40000 some\0"; printf
+# > 08687c1be8a39bde242c31d308baa4aba277dc02 | xxd -p -r) | sha1sum
+# > 62296ca7563b5d575acb0a914442f78f3a76db4d
+# > 
+# > $ # Ask git for the commit hash, then recreate it two different ways
+# > $ git rev-parse HEAD
+# > bfce3b33968e8735e722754ceb89c8756454df1a
+# > $ GIT_AUTHOR_NAME="John Doe" GIT_AUTHOR_EMAIL="john@doe.com"
+# > GIT_AUTHOR_DATE="1234567890" GIT_COMMITTER_NAME="John Doe"
+# > GIT_COMMITTER_EMAIL="john@doe.com" GIT_COMMITTER_DATE="1234567890" git
+# > commit-tree -m "Lovely commit message"
+# > 62296ca7563b5d575acb0a914442f78f3a76db4
+# > bfce3b33968e8735e722754ceb89c8756454df1a
+# > $ printf "commit 168\0tree
+# > 62296ca7563b5d575acb0a914442f78f3a76db4d\nauthor John Doe
+# > <john@doe.com> 1234567890 -0800\ncommitter John Doe <john@doe.com>
+# > 1234567890 -0800\n\nLovely commit message\n" | sha1sum
+# > bfce3b33968e8735e722754ceb89c8756454df1a  -
+# 
+# OK, given an outer-tree hash for a commit and commit metadata I think I can see how
+# to compute the commit hash  for a commit. You last bullet point abouthow the ancestor
+# hashes are included is significant.
+# 
+# Can you unpack the tree-hash computation some more?  Maybe with an example 
+# including two files?
+# 
+# > Some notes:
+# > 
+# >   * The number before the first null byte is the number of characters
+# >     after the first null byte.  Yes, it's redundant information but
+# >     allows git to quickly determine the size of objects.
+# 
+# Got it.
+# 
+# >   * No newlines or anything separating multiple files in a tree object;
+# >     the space character, nul character, and known length of a sha1sum
+# >     are all that are needed.
+# 
+# OK.
+# 
+# >   * Although ls-tree output always shows six characters for object modes,
+# >     leading zeros are stripped when writing into a tree object.
+# 
+# Got that too.
+# 
+# >   * Any parents to record in commit objects are listed on separate lines
+# >     between the tree and the author.  Use 'git cat-file -p' on any commit
+# >     in a repo of your choosing to see an example.
+# 
+# Ah, that is important and I think I understand it.
+
 # end
