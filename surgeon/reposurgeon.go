63a242c9a12972d75a11927b4de426e9d4b955b5
Eric S. Raymond <esr@thyrsus.com>
2020-04-16 14:19:41 -0400

    Add another report to sizeof.

--- surgeon/reposurgeon.go
+++ surgeon/reposurgeon.go
@@ -17450,6 +17450,7 @@ func (rs *Reposurgeon) DoSizeof(lineIn string) bool {
 	fmt.Fprintf(control.baton, "int:               %3d\n", unsafe.Sizeof(0))
 	fmt.Fprintf(control.baton, "map[string]string: %3d\n", unsafe.Sizeof(make(map[string]string)))
 	fmt.Fprintf(control.baton, "[]string:          %3d\n", unsafe.Sizeof(make([]string, 0)))
+	fmt.Fprintf(control.baton, "map[string]bool:  %3d\n", unsafe.Sizeof(make(map[string]bool)))
 	seq := NewNameSequence()
 	fmt.Fprintf(control.baton, "raw modulus:      %-5d\n", len(seq.color)*len(seq.item))
 	fmt.Fprintf(control.baton, "modulus/phi:      %-5d\n", int((float64(len(seq.color)*len(seq.item)))/phi))
