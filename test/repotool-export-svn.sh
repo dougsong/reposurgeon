259cd57604a53f1f299e693d75e157bcfb9216af
Eric S. Raymond <esr@thyrsus.com>
2020-05-06 16:09:27 -0400

    Reinstate the export test for SVN.
    Add a nite to INALL.adoc about possible failure due to version skew.
    
    The following gorepotool test also passes:
    
    REPOTOOL=gorepotool sh repotool-export-svn.sh --regress

--- test/repotool-export-svn.sh
+++ test/repotool-export-svn.sh
@@ -1,8 +1,9 @@
+#!/bin/sh
 ## Test repotool export of svn repo
 
 command -v svn >/dev/null 2>&1 || { echo "    Skipped, svn missing."; exit 0; }
 
-trap "rm -rf /tmp/test-repo$$ /tmp/out$$" 0 12 2 15
+trap 'rm -rf /tmp/test-repo$$ /tmp/out$$' EXIT HUP INT QUIT TERM
 
 ./svn-to-svn -q -n /tmp/test-repo$$ <vanilla.svn
 # This test can fail spuriously due to format skew.  Kevin Caswick
@@ -11,13 +12,15 @@ trap "rm -rf /tmp/test-repo$$ /tmp/out$$" 0 12 2 15
 # > 1.6.11 as the dump is sorted differently, moving svn:log before
 # > svn:author instead of after svn:date. It works fine on svnadmin,
 # > version 1.8.10.
-(cd /tmp/test-repo$$ >/dev/null; ${REPOTOOL:-repotool} export) >/tmp/out$$
+(cd /tmp/test-repo$$ >/dev/null || (echo "$0: cd failed" >&2; exit 1); ${REPOTOOL:-repotool} export) >/tmp/out$$
 
 case $1 in
     --regress)
-        diff --text -u $2.chk /tmp/out$$ || exit 1; ;;
+        diff --text -u repotool-export-svn.chk /tmp/out$$  || (echo "$0: cd failed" >&2; exit 1) || (echo "$0: FAILED"; exit 1); ;;
     --rebuild)
-	cat /tmp/out$$ >$2.chk;;
+	cat /tmp/out$$ >repotool-export-svn.chk;;
+    --view)
+	cat /tmp/out$$;;
 esac
 
 #end
