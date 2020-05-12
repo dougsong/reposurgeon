13d34e183a67bf943982fee821e41aae40805ccd
Eric S. Raymond <esr@thyrsus.com>
2020-05-12 17:23:19 -0400

    Factor stringSet code out of repotool.

--- tool/repotool.go
+++ tool/repotool.go
@@ -24,87 +24,6 @@ import (
 // TMPDIR is the temporary directory under which to perform checkouts
 var TMPDIR string
 
-// FIXME: Partial copy of the set type from reposurgeon. Should be factored
-
-type stringSet struct {
-	store map[string]bool
-}
-
-func newStringSet(elements ...string) stringSet {
-	var ns stringSet
-	ns.store = make(map[string]bool, 0)
-	for _, el := range elements {
-		ns.store[el] = true
-	}
-	return ns
-}
-
-// Iterare bares the underlying map so we can iterate over its keys
-func (s stringSet) Iterate() map[string]bool {
-	return s.store
-}
-
-func (s stringSet) Contains(item string) bool {
-	return s.store[item]
-}
-
-func (s *stringSet) Remove(item string) {
-	delete(s.store, item)
-}
-
-func (s *stringSet) Add(item string) {
-	s.store[item] = true
-}
-
-func (s stringSet) Subtract(other stringSet) stringSet {
-	diff := newStringSet()
-	for item := range s.store {
-		if !other.store[item] {
-			diff.store[item] = true
-		}
-	}
-	return diff
-}
-
-func (s stringSet) Intersection(other stringSet) stringSet {
-	intersection := newStringSet()
-	for item := range s.store {
-		if other.store[item] {
-			intersection.store[item] = true
-		}
-	}
-	return intersection
-}
-
-func (s stringSet) Union(other stringSet) stringSet {
-	// Naive O(n**2) method - don't use on large sets if you care about speed
-	union := newStringSet()
-	for item := range s.store {
-		union.store[item] = true
-	}
-	for item := range other.store {
-		union.store[item] = true
-	}
-	return union
-}
-
-func (s stringSet) Equal(other stringSet) bool {
-	if len(s.store) != len(other.store) {
-		return false
-	}
-	for item := range s.store {
-		if !other.store[item] {
-			return false
-		}
-	}
-	for item := range other.store {
-		if !s.store[item] {
-			return false
-		}
-	}
-	return true
-}
-
 func (s stringSet) Listify() []string {
 	ordered := make([]string, len(s.store))
 	i := 0
@@ -114,14 +33,6 @@ func (s stringSet) Listify() []string {
 	}
 	sort.Strings(ordered)
 	return ordered
-}
-
-func (s stringSet) Empty() bool {
-	return len(s.store) == 0
-}
-
-func (s stringSet) Len() int {
-	return len(s.store)
 }
 
 type squishyParts struct {
