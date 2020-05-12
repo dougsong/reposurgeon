05c0411c9faf7b6741558c406bcbc7a6709b58b6
Eric S. Raymond <esr@thyrsus.com>
2020-05-12 17:18:43 -0400

    Factor out the stringSet class.

--- surgeon/reposurgeon.go
+++ surgeon/reposurgeon.go
@@ -470,74 +470,12 @@ func (s orderedStringSet) toStringSet() stringSet {
 	return out
 }
 
-type stringSet struct {
-	store map[string]bool
-}
-
-func newStringSet(elements ...string) stringSet {
-	var ns stringSet
-	ns.store = make(map[string]bool, 0)
-	for _, el := range elements {
-		ns.store[el] = true
-	}
-	return ns
-}
-
-// Elements bares the underlying map so we can iterate over its keys
-func (s stringSet) Iterate() map[string]bool {
-	return s.store
-}
-
 func (s stringSet) Ordered() orderedStringSet {
 	oset := newOrderedStringSet()
 	for item := range s.store {
 		oset.Add(item)
 	}
 	return oset
-}
-
-func (s stringSet) Contains(item string) bool {
-	return s.store[item]
-}
-
-func (s *stringSet) Remove(item string) {
-	delete(s.store, item)
-}
-
-func (s *stringSet) Add(item string) {
-	s.store[item] = true
-}
-
-func (s stringSet) Subtract(other stringSet) stringSet {
-	diff := newStringSet()
-	for item := range s.store {
-		if !other.store[item] {
-			diff.store[item] = true
-		}
-	}
-	return diff
-}
-
-func (s stringSet) Intersection(other stringSet) stringSet {
-	intersection := newStringSet()
-	for item := range s.store {
-		if other.store[item] {
-			intersection.store[item] = true
-		}
-	}
-	return intersection
-}
-
-func (s stringSet) Union(other stringSet) stringSet {
-	// Naive O(n**2) method - don't use on large sets if you care about speed
-	union := newStringSet()
-	for item := range s.store {
-		union.store[item] = true
-	}
-	for item := range other.store {
-		union.store[item] = true
-	}
-	return union
 }
 
 func (s stringSet) toOrderedStringSet() orderedStringSet {
@@ -559,31 +497,6 @@ func (s stringSet) String() string {
 	// this is used in regression tests.
 	// It doesn't need to be fast.
 	return s.toOrderedStringSet().String()
-}
-
-func (s stringSet) Equal(other stringSet) bool {
-	if len(s.store) != len(other.store) {
-		return false
-	}
-	for item := range s.store {
-		if !other.store[item] {
-			return false
-		}
-	}
-	for item := range other.store {
-		if !s.store[item] {
-			return false
-		}
-	}
-	return true
-}
-
-func (s stringSet) Empty() bool {
-	return len(s.store) == 0
-}
-
-func (s stringSet) Len() int {
-	return len(s.store)
 }
 
 // A copy of the orderedStringSet code with the names changed to protect the innocent.
